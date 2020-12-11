76943018de6fdd49c0aee360c5801540621916f4
Scott Bezek <scottbez1@gmail.com>
2020-12-11 00:15:37 -0800

    Workbee machine definition

--- Grbl_Esp32/src/Config.h
+++ Grbl_Esp32/src/Config.h
@@ -93,12 +93,11 @@ const int MAX_N_AXIS = 6;
 #define BAUD_RATE 115200
 
 //Connect to your local AP with these credentials
-//#define CONNECT_TO_SSID  "your SSID"
-//#define SSID_PASSWORD  "your SSID password"
+#include "ConfigIgnored.h"
 //CONFIGURE_EYECATCH_BEGIN (DO NOT MODIFY THIS LINE)
-#define ENABLE_BLUETOOTH  // enable bluetooth
+// #define ENABLE_BLUETOOTH  // enable bluetooth
 
-#define ENABLE_SD_CARD  // enable use of SD Card to run jobs
+// #define ENABLE_SD_CARD  // enable use of SD Card to run jobs
 
 #define ENABLE_WIFI  //enable wifi
 
@@ -107,11 +106,11 @@ const int MAX_N_AXIS = 6;
 #endif
 
 #define ENABLE_HTTP                //enable HTTP and all related services
-#define ENABLE_OTA                 //enable OTA
-#define ENABLE_TELNET              //enable telnet
-#define ENABLE_TELNET_WELCOME_MSG  //display welcome string when connect to telnet
-#define ENABLE_MDNS                //enable mDNS discovery
-#define ENABLE_SSDP                //enable UPNP discovery
+// #define ENABLE_OTA                 //enable OTA
+// #define ENABLE_TELNET              //enable telnet
+// #define ENABLE_TELNET_WELCOME_MSG  //display welcome string when connect to telnet
+// #define ENABLE_MDNS                //enable mDNS discovery
+// #define ENABLE_SSDP                //enable UPNP discovery
 #define ENABLE_NOTIFICATIONS       //enable notifications
 
 #define ENABLE_SERIAL2SOCKET_IN
